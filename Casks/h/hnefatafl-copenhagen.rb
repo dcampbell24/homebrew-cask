d8f53a3a5cb0f2ca83eaa56532b3f9b64b519297
David Campbell <noreply@hnefatafl.org>
2026-04-27 00:42:10 -0400

    Iniial version of hnefatafl-copenhagen.

---
+++ Casks/h/hnefatafl-copenhagen.rb
@@ -0,0 +1,11 @@
+cask "hnefatafl-copenhagen" do
+  version "5.8.0"
+  sha256 "92f8775e3701410c8e09d55b0d62d23256109a74ac9c67466bdffdf1dbe8f1a8"
+
+  url "https://hnefatafl.org/homebrew/hnefatafl-copenhagen.tar.gz"
+  name "Hnefatafl Copenhagen"
+  desc "Client that connects to a server"
+  homepage "https://hnefatafl.org/"
+
+  app "hnefatafl-copenhagen.app"
+end
