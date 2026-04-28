b414ab7d11b5112cf9d0c4f062b13a8390a47ab5
David Campbell <noreply@hnefatafl.org>
2026-04-27 21:58:31 -0400

    Set :no_check for sha256, add depends_on.

--- Casks/h/hnefatafl-copenhagen.rb
+++ Casks/h/hnefatafl-copenhagen.rb
@@ -1,11 +1,13 @@
 cask "hnefatafl-copenhagen" do
   version "5.8.0"
-  sha256 "92f8775e3701410c8e09d55b0d62d23256109a74ac9c67466bdffdf1dbe8f1a8"
+  sha256 :no_check
 
   url "https://hnefatafl.org/homebrew/hnefatafl-copenhagen.tar.gz"
   name "Hnefatafl Copenhagen"
   desc "Client that connects to a server"
   homepage "https://hnefatafl.org/"
+
+  depends_on macos: ">= :big_sur"
 
   app "hnefatafl-copenhagen.app"
 end
